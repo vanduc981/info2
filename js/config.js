d5e799f75fdc1cc133738857f0e8141f5b66e449
VAN DUC <dvan98679@gmail.com>
2023-01-08 12:34:26 +0700

    Release suntech frontend final project

---
+++ js/config.js
@@ -0,0 +1,52 @@
+const USER = {
+    banks: [{
+            name: "MB BANK",
+            logo: "./images/mbbank.png",
+            number: "77788899992007",
+            owner: "NGUYEN VAN DUC"
+        },
+        {
+            name: "BIDV BANK",
+            logo: "./images/bidv.png",
+            number: "443110001644642",
+            owner: "NGUYEN VAN DUC"
+        },
+        {
+            name: "AGRI BANK",
+            logo: "./images/agribank.png",
+            number: "xxxxxxxxxxxx",
+            owner: "NGUYEN VAN DUC"
+        },
+        {
+            name: "PAYPAL",
+            logo: "./images/paypal.png",
+            number: "nguyenvanducvn",
+            owner: "NGUYEN VAN DUC"
+        },
+        {
+            name: "MOMO",
+            logo: "./images/momo.jpeg",
+            number: "0332882XXX",
+            owner: "NGUYEN VAN DUC",
+            fastTransfer: true
+        },
+        {
+            name: "ZALO PAY",
+            logo: "./images/zalopay.png",
+            number: "0332882XXX",
+            owner: "NGUYEN VAN DUC"
+        },
+        {
+            name: "VIETTEL PAY",
+            logo: "./images/viettelpay.png",
+            number: "0332882XXX",
+            owner: "NGUYEN VAN DUC"
+        },
+        {
+            name: "SHOPEE PAY",
+            logo: "./images/shopeepay.png",
+            number: "xxxxxxxxxxxx",
+            owner: "NGUYEN VAN DUC"
+        },
+    ]
+};
